33472460a6521273b3b496a8b588063322b61213
She-Codes <n.y.shaw@gmail.com>
2014-04-21 22:00:25 -0400

    still setting up models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416001951) do
+ActiveRecord::Schema.define(version: 20140422015418) do
 
   create_table "categories", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
@@ -35,7 +37,9 @@ ActiveRecord::Schema.define(version: 20140416001951) do
   end
 
   create_table "users", force: true do |t|
-    t.string "username"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

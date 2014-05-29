4b3cb021ca45c524e7c15e557554d4678fd23cbc
She-Codes <n.y.shaw@gmail.com>
2014-05-28 20:57:26 -0400

    working on categories

--- app/controllers/concerns/categories_controller.rb
+++ app/controllers/concerns/categories_controller.rb
@@ -1,27 +1,27 @@
 class CategoriesController < ApplicationController
 
-  def index
-    @categories = Category.all
-  end
-
   def new
     @category = Category.new
   end
 
   def create
-    @category = Category.new(post_params)
+    @category = Category.new(category_params)
 
     if @category.save
-      flash[:notice] = "Your category was created."
-      redirect_to categories_path
+      flash[:notice] = "Category has been created."
+      redirect_to root_path
     else
       render 'new'
     end
   end
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   private
 
-  def post_params
+  def category_params
     params.require(:category).permit(:name)
   end
 

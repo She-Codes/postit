3677e8e99cf66bbcf710da34ec50ceeb554d3336
She-Codes <n.y.shaw@gmail.com>
2014-07-15 19:05:44 -0400

    require_same_user approach didn't work. removed edit post link from post index page and added condition to show edit link on show post page only if current user is post creator.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,7 @@ class UsersController < ApplicationController
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update]
   
-  def show
-
-  end
+  def show; end
 
   def new
     @user = User.new
@@ -45,4 +43,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
+    end
+  end
 end

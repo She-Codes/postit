2033fec367a008622e8fecfd8284947b003abd05
She-Codes <n.y.shaw@gmail.com>
2014-09-27 13:23:46 -0400

    commiting changes made before a long break

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,18 +9,36 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
 
-  def generate_slug
-    possible_slug = self.title.gsub(' ', '-').downcase
-    posts_with_slug = Post.where("slug = '#{possible_slug}'")
-    num_of_posts_with_slug = posts_with_slug.length
-    if num_of_posts_with_slug > 0
-      self.slug = "#{possible_slug}-#{num_of_posts_with_slug + 1}"
-    else
-      self.slug = possible_slug
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by(slug: the_slug)
+    count = 2
+
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by(slug: the_slug)
+      count += 1
     end
+
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
   end
 
   def to_param

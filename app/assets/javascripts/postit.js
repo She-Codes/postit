03aa765708fc09b74f80d402273955f6044f0831
She-Codes <n.y.shaw@gmail.com>
2014-11-29 12:25:19 -0500

    styled sidebar, added js for buttons and showing categories

---
+++ app/assets/javascripts/postit.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('.view-cats').click(function(e){
+    e.preventDefault();
+    $(this).addClass('hide');
+    $('.expanded-view-cat').addClass('show');
+    $('.hide-cats').addClass('show');
+  });
+
+  $('.hide-cats').click(function(e){
+    e.preventDefault();
+    $(this).removeClass('show');
+    $('.expanded-view-cat').removeClass('show');
+    $('.view-cats').removeClass('hide');
+  });
+
+  $('.user-options-button').click(function(e){
+    e.preventDefault();
+    if ( $(this).hasClass('show') ){
+      $(this).removeClass('show');
+    } else {
+      $(this).addClass('show');
+    }
+
+    if ( $('.user-links').hasClass('show') ){
+      $('.user-links').removeClass('show');
+    } else {
+      $('.user-links').addClass('show');
+    }
+  })
+});
